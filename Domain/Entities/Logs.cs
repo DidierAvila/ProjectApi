2b454223195a628dc07e4163a49aee94fac607b2
Didier Avila <didier_avila9c@ehotmail.com>
2025-07-30 22:48:23 -0500

    Ajuste

--- Domain/Entities/Logs.cs
+++ Domain/Entities/Logs.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
-    [Table(name: "Post")]
+    [Table(name: "Logs")]
     public partial class Logs
     {
         [Key]
